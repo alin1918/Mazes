18cab13754fd0d2b68b042650c7d613129c24cb2
Alin Balan <alin@white-hats.ro>
2012-02-13 22:56:58 +0200

    changes again

--- class/loader.php
+++ class/loader.php
@@ -10,14 +10,52 @@ class loader {
 		return self::$_instance;	
 	}
 	
-	public function load($mazename, $start = 0, $end = 0) 
+	public function load($mazename, $start = array(), $end = array()) 
 	{
-	   worker::getInstance()->getMatrix($mazename);
+		$maze =    worker::getInstance()->getMatrix($mazename);
+		$result = $this->solveMaze($data,$start,$end);
 /*
 		if(empty(cache::getInstance()->load($mazename))) {
 				
 		   }
-*/		
+*/		if(DEBUG) {
+		  $this->displayMaze($maze['data']);
+		}	
+	}
+
+
+	public function solveMaze($data, $start, $end) {
+		$result = array();		
+		$chances = array();
+		$deadPoints = array();
+		
+
+		return $result;
+	}
+
+	public function paintRoad($data,$road)
+	{
+
+
+	}
+	public function displayMaze($data)
+	{
+		$colsize = count ($data);
+		echo '<table cellspacing="0" border="1">';
+		for($i = 0; $i< $colsize; $i++) {
+			echo '<tr>';
+			$rowsize = count ($data[$i]);
+			for($j = 0; $j < $rowsize; $j++) {
+				if($data[$i][$j] == 1) {
+					echo '<td style="background-color:green;font-size:6px;width:2px;height:2px;font-size:2px;" data="i:',$i,',j:',$j,'"></td>';	
+				} else {	
+					echo '<td style="background-color:red";font-size:6px;width:2px;height:2px;font-size:2px;" data="i:',$i,',j:',$j,'"></td>';	
+				}
+			}
+			echo '</tr>';	
+		}
+		echo '</table>';
+		die;
 	}
 }
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
 
 define ('BASEDIR', dirname(__FILE__));
+define ('DEBUG' , true);
 
 include(BASEDIR . '/config.php');
 
 $_GET['maze'] = 'rastert_feature1.txt';
-$_GET['start'] = '0,0';
-$_GET['end'] = '5,4';
+$_GET['start'] = '0,238';
+$_GET['end'] = '105,344';
 
 
+if(!isset($_GET['start']) || !isset($_GET['end']) || !isset($_GET['maze'])) {
+	die('You need to provide a maze name a starting point and an end point');	
+}
+$start = $_GET['start'];
+$end = $_GET['end'];
+
+list($sx,$sy) = explode(',',$start);
+list($ex,$ey) = explode(',',$end);
+
+if(is_null($sx) || is_null($sy) || is_null($ex) || is_null($ey)){
+	die('You need to provide a valid starting and ending point');
+}
 
 try {
-loader::getInstance()->load($_GET['maze']);
+loader::getInstance()->load($_GET['maze'], array('x'=>$sx, 'y'=>$sy),array('x'=> $ex, 'y'=> $ey));
 } catch( Exception $e) {
 	echo $e->getMessage();
 }
